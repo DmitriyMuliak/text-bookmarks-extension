f71a2b8f8207b462e7984165b4e5bc405f051aef
Your Name <dmitriymuliak@gmail.com>
2025-01-08 09:00:00 +0200

    feat: add shared constants for storage keys and UI element IDs

---
+++ src/shared/constants/index.ts
@@ -0,0 +1,5 @@
+export const STORAGE_KEY = 'text_bookmarks_v1' as const
+export const STORAGE_VERSION = 1 as const
+export const HIGHLIGHT_DURATION = 2000 as const // ms
+export const SAVE_BTN_ID = '__tbm-save-btn__' as const
+export const HIGHLIGHT_MARK_ID = '__tbm-highlight__' as const
